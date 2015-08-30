a92e452cf61ee736b9a1c9849a259802aeec8033
Jack <haoran.sun.1993@gmail.com>
2015-08-30 18:03:57 -0400

    1
    1

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+$(window).resize(function() {
+	//get width
+	//get height of 
+	
+	$('.theClassName').css('display', 'inline')
+}
+);
+
+//submenu
+$( "#outer" ).mouseleave(function() {
+  $( "#log" ).append( "<div>Handler for .mouseleave() called.</div>" );
+});
