d8828f6d72295681f33674d1acd99e5e632e9a1b
Jack <haoran.sun.1993@gmail.com>
2015-11-03 07:18:56 -0500

    adding angularJS framework

---
+++ controller/controller.js
@@ -0,0 +1,10 @@
+
+angular.module('spaceconcordia_website', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
