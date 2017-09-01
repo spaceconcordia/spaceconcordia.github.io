305a4ff615c7d91a1fdb090288d0b489a1605c33
Peter <petergranitski@gmail.com>
2017-09-01 09:22:40 -0400

    fixed about navbar/dropdown/link to redirect to index.html

--- expandableLinks.js
+++ expandableLinks.js
@@ -5,38 +5,44 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 	var lists = document.querySelectorAll('li.dropdown')
+	lists[0].classList.add('about-btn-js');
 	lists[1].classList.add('spacecraft-btn-js');
 	lists[2].classList.add('rocketry-btn-js');
 	lists[3].classList.add('projects-btn-js');
 	lists[4].classList.add('contact-btn-js');
 
 	// get nav buttons
-	var spaceCraftBtn = document.querySelectorAll('.spacecraft-btn-js')[0];
-	var rocketryBtn = document.querySelectorAll('.rocketry-btn-js')[0];
-	var projectsBtn = document.querySelectorAll('.projects-btn-js')[0];
-	var contactBtn = document.querySelectorAll('.contact-btn-js')[0];
+	var aboutBtn 		= document.querySelectorAll('.about-btn-js')[0];
+	var spaceCraftBtn 	= document.querySelectorAll('.spacecraft-btn-js')[0];
+	var rocketryBtn 	= document.querySelectorAll('.rocketry-btn-js')[0];
+	var projectsBtn 	= document.querySelectorAll('.projects-btn-js')[0];
+	var contactBtn 		= document.querySelectorAll('.contact-btn-js')[0];
 
 	// event handler functions
+	var navigateToAbout = function () {
+		window.location.href = 'index.html';
+	}
 	var navigateToSpaceCraft = function() {
-		window.location.href = "spacecraft.html";
+		window.location.href = 'spacecraft.html';
 	}
 
 	var navigateToRocketry = function() {
-		window.location.href = "rocketry.html";
+		window.location.href = 'rocketry.html';
 	}
 
 	var navigateToProjects = function() {
-		window.location.href = "projects.html";
+		window.location.href = 'projects.html';
 	}
 
 	var navigateToContact = function() {
-		window.location.href = "contact.html";
+		window.location.href = 'contact.html';
 	}
 
 	// bind listeners to dropdown button hybrids
-	spaceCraftBtn.onclick = navigateToSpaceCraft;
-	rocketryBtn.onclick = navigateToRocketry;
-	projectsBtn.onclick = navigateToProjects;
-	contactBtn.onclick = navigateToContact;
+	aboutBtn.onclick 		= navigateToAbout;
+	spaceCraftBtn.onclick 	= navigateToSpaceCraft;
+	rocketryBtn.onclick 	= navigateToRocketry;
+	projectsBtn.onclick 	= navigateToProjects;
+	contactBtn.onclick 		= navigateToContact;
 });
 
