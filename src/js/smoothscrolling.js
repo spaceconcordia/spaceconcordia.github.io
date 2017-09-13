fd57de7c54edad434597127c37320f857687bbdb
Peter <petergranitski@gmail.com>
2017-09-13 00:20:49 -0400

    fixed smoothscrolling.js

--- src/js/smoothscrolling.js
+++ src/js/smoothscrolling.js
@@ -1,12 +1,15 @@
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
+$(document).ready(function() {
+    $('a[href="#followus"]').click(function() {
+        
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            
+            return false;
+        }
+    });
 });
