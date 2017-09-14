47124a7e7cbb6b06878947249e541a5bdb09f943
Peter <petergranitski@gmail.com>
2017-09-14 03:03:59 -0400

    added smooth scroll up for back to top button in right bottom corner of footer

--- src/js/smoothscrolling.js
+++ src/js/smoothscrolling.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
-    $('a[href="#followus"]').click(function() {
-        
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
+    if ($('a[href="#followus"]').length === 1) {
+        $('a[href="#followus"]').click(function() {
             
-            return false;
-        }
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                
+                return false;
+            }
+        });
+    }
+
+    $('footer').find('a[href="#"]').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+        
+        return false;
     });
 });
